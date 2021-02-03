f92af1169d03d4804947ffd7460e7bc357206927
Ivan Zhytkevych <ipython10@gmail.com>
2021-02-03 22:09:23 +0200

    static fix

--- src/mercury/settings.py
+++ src/mercury/settings.py
@@ -133,8 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 AUTH_USER_MODEL = 'users.CustomUser'
