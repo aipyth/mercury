7498e578129a60e6d17a2a6045c8557ebc3cf573
Ivan <aipyth.func@gmail.com>
2021-02-27 01:05:13 +0200

    paginated api output. functests start. golang bot start. docker images optimization.

--- src/bot/go.mod
+++ src/bot/go.mod
@@ -1,3 +1,9 @@
 module mercury-bot
 
 go 1.14
+
+require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.6.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.3.5 // indirect
+)
--- src/bot/server.go
+++ src/bot/server.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
 
 func main() {
-	fmt.Println("Server started")
+	if os.Getenv("BOT_TOKEN") == "" {
+		return
+	}
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:  os.Getenv("BOT_TOKEN"),
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hello World!")
+	})
+
+	b.Handle(tb.OnQuery, func (q* tb.Query) {
+		if q.Text != "" {
+			log.Println("Getting concrete room")
+		} else {
+			// empty request so we list awailable rooms
+			log.Println("List awailable rooms")
+		}
+	})
+
+	b.Start()
+
 }
