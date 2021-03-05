1e14cb15f09b119048100711760e2d6963c61359
Ivan <aipyth.func@gmail.com>
2021-03-05 20:03:46 +0200

    staticfiles and env var in settings.py fix

--- src/api/mercury/settings.py
+++ src/api/mercury/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'postgres'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('HOST', 'db'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': 5432,
     }
 }
